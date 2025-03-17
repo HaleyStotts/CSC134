acff276baf5b3d78fbf6c2267e52da8e8eff32bd
Haley Stotts <hlbstotts@gmail.com>
2025-03-17 00:56:30 +0000

    m4t1p1done

---
+++ M4T1.cpp
@@ -0,0 +1,20 @@
+// Haley Stotts
+// CSC 134
+// March 16, 2025
+// M4T1
+
+//Part 1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0; // Initialize counter
+
+    while (count < 5) { // Loop will execute 5 times
+        cout << "Hello" << endl;
+        count++; // Increment the counter
+    }
+
+    return 0;
+}
