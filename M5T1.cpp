8adf88c532de8cf3c3b1c24e16a4c8e109164267
Haley Stotts <hlbstotts@gmail.com>
2025-04-07 03:42:44 +0000

    M5T1DONE

---
+++ M5T1.cpp
@@ -0,0 +1,30 @@
+//Haley Stotts
+//CSC 134
+//April 6 2025
+//M5T1
+
+#include <iostream>
+#include <string>
+using namespace std;
+// declare our functions
+string formatAnswer(int answer);
+void printAnswer(string msg);
+int main() {
+int answer = 5;
+string message;
+message = formatAnswer(answer);
+printAnswer(message);
+return 0;
+}
+// define functions
+string formatAnswer (int answer) {
+// Make a nice looking string containing the answer
+string answerMessage;
+answerMessage = "The answer is ";
+answerMessage += to_string(answer);
+return answerMessage;
+}
+void printAnswer(string msg) {
+// display our message
+cout << msg << endl;
+}
